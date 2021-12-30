9af3634511446d7f52e7c205e96840c611ce6916
bernygena93 <bernygena93@users.noreply.github.com>
2021-12-30 10:53:01 -0300

    deployed in heroku from github

--- bin/mongodb.js
+++ bin/mongodb.js
@@ -3,7 +3,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://bernygena:Bernygena93@db-heroku.xjwq6.mongodb.net/test",
+  "mongodb+srv://bernygena:Bernygena93@db-heroku.xjwq6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   { useNewUrlParser: true },
   function (error) {
     if (error) {
